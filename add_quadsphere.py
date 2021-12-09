6d95f7446997462717518f8562dc1e5c59373d86
tijlp <tijlprinssen@gmail.com>
2021-12-09 14:36:52 +0100

    added subdiv prop

--- add_quadsphere.py
+++ add_quadsphere.py
@@ -17,7 +17,7 @@ bl_info = {
 class QS_OP_add_quadsphere(bpy.types.Operator):
     bl_idname =  "mesh.quadsphere_add"
     bl_label =  "Quadsphere"
-    bl_description =  "Add a sphere with proper quad topology"
+    bl_description =  """Add a sphere with proper quad topology"""
     bl_options =  {'REGISTER', 'UNDO'}
 
     size: FloatProperty(
@@ -25,9 +25,16 @@ class QS_OP_add_quadsphere(bpy.types.Operator):
         description = "Size",
         default = 2.0,
         min = 0.01,
-        soft_max = 10
+        soft_max = 5
     )
- 
+    subdiv_level: IntProperty(
+        name = "Subdivisions",
+        description="Levels of Subdivision",
+        default=4,
+        min=0,
+        soft_max=6,
+        max=10
+    )
     apply_modifiers: BoolProperty(
         name =  "Apply Modifiers",
         description =  "Make mesh real",
@@ -37,31 +44,49 @@ class QS_OP_add_quadsphere(bpy.types.Operator):
     
     def draw(self, context):
         layout = self.layout
-        layout.prop(self, "size")    
-        
-        # layout.prop(self, "show_wire")
+        obj = context.object   
+        view = context.space_data      
 
-        layout.separator()            
-        layout.prop(self, "apply_modifiers", icon="CHECKMARK")
+        layout.prop(self, "size")    
+        layout.prop(self, "subdiv_level")    
+        
+        layout.separator()
+        row = layout.row()
+        # layout.prop(view.overlay, "show_wireframes")            
+        row.prop(self, "apply_modifiers", icon="CHECKMARK")
 
 
     def execute(self, context):
+        # add cube
         bpy.ops.mesh.primitive_cube_add(size=self.size, calc_uvs=True, align='WORLD')
-        bpy.ops.object.modifier_add(type='SUBSURF')
-        context.active_object.modifiers["Subdivision"].levels = 4
+
+        # set subdiv 
+        object = context.object
+        self.add_subdivision_modifier(object, self.subdiv_level)
+
+        # set render subdiv levels equal to viewport
+        subdiv_levels = context.active_object.modifiers["Subdivision"].levels         
+        context.active_object.modifiers["Subdivision"].render_levels = subdiv_levels
+
+        # cast to sphere
         bpy.ops.object.modifier_add(type='CAST')
         context.active_object.modifiers["Cast"].factor = 1
         bpy.ops.object.shade_smooth()
         
-        # set render subdiv levels
-        subdiv_levels = context.active_object.modifiers["Subdivision"].levels         
-        context.active_object.modifiers["Subdivision"].render_levels = subdiv_levels
-
+        # apply modifiers
         if self.apply_modifiers:
             bpy.ops.object.modifier_apply(modifier="Subdivision")
             bpy.ops.object.modifier_apply(modifier="Cast")
 
         return {'FINISHED'}
+
+
+
+    def add_subdivision_modifier(self, object, subdiv_level):
+        mod = object.modifiers.new(name="Subdivision", type="SUBSURF")
+        mod.subdivision_type = 'CATMULL_CLARK'
+        mod.levels = subdiv_level
+
 
 
 def add_quadsphere_to_menu(self, context):
