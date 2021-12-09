7a77d2a32ad80ca2ed99368f3a4c28573d9c5a61
tijlp <tijlprinssen@gmail.com>
2021-12-09 20:32:29 +0100

    Added viewport color picker menu

--- add_quadsphere.py
+++ add_quadsphere.py
@@ -1,11 +1,11 @@
 import bpy
-from bpy.props import BoolProperty, FloatProperty, IntProperty
+from bpy.props import BoolProperty, FloatProperty, IntProperty, FloatVectorProperty
 
 bl_info = {
     "name": "Quadsphere",
     "description": "Turn a cube into a sphere with proper quad topology. Shift+A to add.. ",
     "author": "Tijl Prinssen",
-    "version": (0, 1, 0),
+    "version": (0, 3, 0),
     "blender": (2, 80, 0),
     "location": "VIEW3D > Add > Mesh",
     "warning": "",
@@ -42,6 +42,8 @@ class QS_OP_add_quadsphere(bpy.types.Operator):
     )
 
     
+
+    
     def draw(self, context):
         layout = self.layout
         obj = context.object   
@@ -51,14 +53,17 @@ class QS_OP_add_quadsphere(bpy.types.Operator):
         layout.prop(self, "subdiv_level")    
         
         layout.separator()
+        
         row = layout.row()
-        # layout.prop(view.overlay, "show_wireframes")            
+        # row.prop(self, 'align')
+        # row.prop(self, "object_color")
+        row.operator("wm.set_viewport_color", text="Viewport Color", icon="MATERIAL")
         row.prop(self, "apply_modifiers", icon="CHECKMARK")
 
 
     def execute(self, context):
         # add cube
-        bpy.ops.mesh.primitive_cube_add(size=self.size, calc_uvs=True, align='WORLD')
+        bpy.ops.mesh.primitive_cube_add(size=self.size, calc_uvs=True)
 
         # set subdiv 
         object = context.object
@@ -72,7 +77,8 @@ class QS_OP_add_quadsphere(bpy.types.Operator):
         bpy.ops.object.modifier_add(type='CAST')
         context.active_object.modifiers["Cast"].factor = 1
         bpy.ops.object.shade_smooth()
-        
+
+           
         # apply modifiers
         if self.apply_modifiers:
             bpy.ops.object.modifier_apply(modifier="Subdivision")
@@ -89,11 +95,67 @@ class QS_OP_add_quadsphere(bpy.types.Operator):
 
 
 
+class QS_OP_add_shader(bpy.types.Operator):
+    bl_idname =  "wm.set_viewport_color" 
+    bl_label = "Viewport Color"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    object_color : FloatVectorProperty(
+        name='Color',
+        subtype='COLOR_GAMMA',
+        size=4,
+        default=(0.5, 0.5, 0.5, 1), 
+        min= 0, 
+        max= 1
+    )
+    object_metallic: FloatProperty(
+    name = "Metallic",
+    description = "Metallic factor, should be set to either 0 or 1 for realism",
+    default = 0,
+    min = 0,
+    max = 1
+    )
+    object_specular: FloatProperty(
+    name = "Specular",
+    description = "Amount of specularity",
+    default = 0,
+    min = 0,
+    max = 1
+    )
+    object_roughness: FloatProperty(
+    name = "Roughness",
+    description = "Amount of roughness",
+    default = 0,
+    min = 0,
+    max = 1
+    )
+
+    def execute(self, context): 
+        # object.data.materials.append(mat)
+        # create viewport material
+        object = bpy.context.active_object
+        mat = bpy.data.materials.new(name="Material")
+        mat.use_nodes = False
+        bpy.context.object.active_material = mat
+        object.active_material.diffuse_color = self.object_color
+        object.active_material.metallic = self.object_metallic
+        object.active_material.specular_intensity = self.object_specular
+        object.active_material.roughness = self.object_roughness
+
+        return {'FINISHED'}
+
+    def invoke(self, context, event):
+        return context.window_manager.invoke_props_dialog(self, width= 210)
+
+
+
+
 def add_quadsphere_to_menu(self, context):
     self.layout.operator('mesh.quadsphere_add', text="Quadsphere", icon="SPHERE")
   
 
 classes = {
+    QS_OP_add_shader,
     QS_OP_add_quadsphere,
 }
 
